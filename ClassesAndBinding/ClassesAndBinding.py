da4d554134257dfbba88b49bd69e3f4f44e5f854
MikaelCMiller <mikael.miller@erieri.com>
2017-09-11 14:50:37 -0700

    Connect SA.Job for YearsMedianSalaryExperience

--- ClassesAndBinding/ClassesAndBinding.py
+++ ClassesAndBinding/ClassesAndBinding.py
@@ -17,7 +17,7 @@ class Datatraverse:
 
 	def inititalizedataframe(self):
 		self.pyocnxn = pyodbc.connect("DRIVER={SQL Server};SERVER=SNADSSQ3;DATABASE=assessorwork;trusted_connection=yes;")
-		self.sql = """SELECT erijobid,jobdot,jobdottitle FROM sa.fname WHERE jobsource = 'E' and jobactive not in (0,3,5,7,9)"""
+		self.sql = """SELECT f.erijobid, jobdot, jobdottitle, years FROM sa.fname f join (select erijobid, case when yearsmediansalaryexperience>40 then 1 else 0 end as years from sa.job) j on j.erijobid=f.erijobid WHERE f.jobsource = 'E' and f.jobactive not in (0,3,5,7,9)"""
 		self.jobsdf = pd.DataFrame(psql.read_sql(self.sql, self.pyocnxn))
 		print(self.jobsdf)
 		print("Dataframe loaded from SQL")
